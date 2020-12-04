580f6872e0f03a766a88f756fcd6d2ab23942c71
Ashley Smith <ash.mash.134@gmail.com>
2020-12-04 17:58:24 +1100

    Added functionality to move lift up and down

--- models/lift.js
+++ models/lift.js
@@ -1,13 +1,20 @@
 class Lift {
   constructor() {
-    this.current_floor = 1;
-    this.instructions = [];
-    this.direction = "";
-    this.floorsToGoTo = [];
+    this.currentFloor = 1;
+    this.initialInstructions = [];
+    this.direction = 'stationary';
   }
 
   addInstructions(newInstructions) {
     return this.instructions.push(newInstructions);
+  }
+
+  moveUp() {
+    return this.currentFloor++;
+  }
+
+  moveDown() {
+    return this.currentFloor--;
   }
 }
 
