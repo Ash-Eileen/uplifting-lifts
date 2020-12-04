279e21dd5a5c305698880fae129753d4c067837e
Ashley Smith <ash.mash.134@gmail.com>
2020-12-04 14:47:51 +1100

    Added file structure and created lift class in lift.js

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const prompts = require("prompts");
+// const prompts = require("prompts");
 
-const instructions = [];
+// const instructions = [];
 
-const addInstructions = async () => {
-  const liftInstructionQuestions = [
-    {
-      type: "number",
-      name: "level",
-      message: "What level was the lift called from?",
-    },
-    {
-      type: "text",
-      name: "direction",
-      message: "Up or down?",
-    },
-  ];
+// const addInstructions = async () => {
+//   const liftInstructionQuestions = [
+//     {
+//       type: "number",
+//       name: "level",
+//       message: "What level was the lift called from?",
+//     },
+//     {
+//       type: "text",
+//       name: "direction",
+//       message: "Up or down?",
+//     },
+//   ];
 
-  const response = await prompts({
-    type: "text",
-    name: "newInstructionsToAdd",
-    message: "New instructions received? (Y or N)",
-  });
+//   const response = await prompts({
+//     type: "text",
+//     name: "newInstructionsToAdd",
+//     message: "New instructions received? (Y or N)",
+//   });
 
-  if (response.newInstructionsToAdd.toUpperCase() === "Y") {
-    const numberOfInstructions = await prompts({
-      type: "number",
-      name: "numberOfInstructions",
-      message: "Please enter the number of instructions received",
-    });
+//   if (response.newInstructionsToAdd.toUpperCase() === "Y") {
+//     const numberOfInstructions = await prompts({
+//       type: "number",
+//       name: "numberOfInstructions",
+//       message: "Please enter the number of instructions received",
+//     });
 
-    while (numberOfInstructions.numberOfInstructions > 0) {
-      const newInstructions = await prompts(liftInstructionQuestions);
-      instructions.push(newInstructions);
-      numberOfInstructions.numberOfInstructions--;
-    }
-  }
-  console.log(instructions);
-};
+//     while (numberOfInstructions.numberOfInstructions > 0) {
+//       const newInstructions = await prompts(liftInstructionQuestions);
+//       instructions.push(newInstructions);
+//       numberOfInstructions.numberOfInstructions--;
+//     }
+//   }
+//   console.log(instructions);
+// };
 
-addInstructions();
+// addInstructions();
---
+++ models/lift.js
@@ -0,0 +1,10 @@
+class Lift {
+  constructor() {
+    this.current_floor = 1;
+    this.instructions = [];
+    this.directon = "";
+    this.floorsToGoTo = [];
+  }
+}
+
+module.exports = Lift;
